0cc435288cb4d12b99b880b11c40fcf2a0074027
saraldor <ch@star78.se>
2021-02-20 15:53:58 +0100

    New Game from me, i just learning |NPC| more and effects

---
+++ src/ai/state/Stand.java
@@ -0,0 +1,19 @@
+package ai.state;
+
+import ai.AITransition;
+import entity.NPC;
+import game.state.State;
+
+public class Stand extends AIState {
+    private int updatesAlive;
+
+    @Override
+    protected AITransition initializeTransition() {
+        return new AITransition("wander", ((state, currentCharacter) -> updatesAlive >= state.getTime().getUpdatesFromSeconds(3)));
+    }
+
+    @Override
+    public void update(State state, NPC currentCharacter) {
+        updatesAlive++;
+    }
+}
