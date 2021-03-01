4344b97d311b3a5d3195192e306f4d1b0a767088
saraldor <ch@star78.se>
2021-03-01 13:47:23 +0100

    Update UI

--- src/ui/UIContainer.java
+++ src/ui/UIContainer.java
@@ -7,24 +7,37 @@ import gfx.ImageUtils;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
-public class UIContainer extends UIComponent {
+public abstract class UIContainer extends UIComponent {
 
-    private Color backgroundColor;
+    protected Color backgroundColor;
+
+    protected List<UIComponent> children;
 
     public UIContainer() {
         super();
-        backgroundColor = new Color(35, 35, 35);
+        backgroundColor = Color.RED;
+        margin = new Spacing(5);
+        children = new ArrayList<>();
         calculateSize();
         calculatePosition();
     }
 
+    protected abstract Size calculateContentSize();
+    protected abstract void calculateContentPosition();
+
     private void calculateSize() {
-        size = new Size(padding.getHorizontal(), padding.getVertical());
+        Size calculatedContentSize = calculateContentSize();
+        size = new Size(
+                padding.getHorizontal() + calculatedContentSize.getWidth(),
+                padding.getVertical() + calculatedContentSize.getHeight());
     }
 
     private void calculatePosition() {
         position = new Position(margin.getLeft(), margin.getTop());
+        calculateContentPosition();
     }
 
     @Override
@@ -35,13 +48,31 @@ public class UIContainer extends UIComponent {
         graphics.setColor(backgroundColor);
         graphics.fillRect(0, 0, size.getWidth(), size.getHeight());
 
+        for(UIComponent uiComponent : children) {
+            graphics.drawImage(
+                    uiComponent.getSprite(),
+                    uiComponent.getPosition().intX(),
+                    uiComponent.getPosition().intY(),
+                    null
+            );
+        }
+
         graphics.dispose();
         return image;
     }
 
     @Override
     public void update(State state) {
+        children.forEach(component -> component.update(state));
         calculateSize();
         calculatePosition();
+    }
+
+    public void addUIComponent(UIComponent uiComponent) {
+        children.add(uiComponent);
+    }
+
+    public void setBackgroundColor(Color color) {
+        backgroundColor = color;
     }
 }
